6199b669c5aa7584730ec8c7753a8659b09e4a3a
G0urmet <60777262+G0urmetD@users.noreply.github.com>
2024-10-15 09:22:10 +0200

    Update update_checker.py
    adding logic, to read version number out of config.json file

--- update_checker.py
+++ update_checker.py
@@ -1,12 +1,18 @@
-import sys
-import requests
-import zipfile
-import io
 import os
+import io
+import sys
+import json
 import shutil
+import zipfile
+import requests
 from colorama import Fore, Style
 
-CURRENT_VERSION = "easySub-v1.2"
+def load_config():
+    with open("config.json") as config_file:
+        return json.load(config_file)
+
+config = load_config()
+CURRENT_VERSION = config["version"]
 
 def check_for_updates():
     """
@@ -31,31 +37,25 @@ def update_tool(latest_version):
     """
     print(f"{Fore.GREEN}[+]{Style.RESET_ALL} Updating the tool ...")
     
-    # Download-URL des ZIP-Archivs
     download_url = f"https://github.com/G0urmetD/easySub/archive/refs/tags/{latest_version}.zip"
     
     try:
         response = requests.get(download_url)
-        response.raise_for_status()  # Werfe Ausnahme bei einem Fehlerstatus
+        response.raise_for_status()
 
-        # Entpacken des ZIP-Archivs
         with zipfile.ZipFile(io.BytesIO(response.content)) as zip_file:
-            zip_file.extractall("easySub_temp")  # Entpacke in ein temporäres Verzeichnis
+            zip_file.extractall("easySub_temp")
             
-        # Dynamisch den ersten Ordner innerhalb von "easySub_temp" finden
         extracted_folder = os.path.join("easySub_temp", os.listdir("easySub_temp")[0])
         
-        # Überprüfen, ob der Ordner existiert
         if not os.path.isdir(extracted_folder):
             raise FileNotFoundError(f"{extracted_folder} does not exist.")
         
-        # Kopiere alle Dateien aus dem entpackten Verzeichnis in das aktuelle Verzeichnis
         for root, dirs, files in os.walk(extracted_folder):
             for file in files:
                 file_path = os.path.join(root, file)
                 destination_path = os.path.join(os.getcwd(), file)
                 
-                # Ersetze die Dateien im aktuellen Verzeichnis
                 shutil.copy2(file_path, destination_path)
                 print(f"{Fore.GREEN}[+]{Style.RESET_ALL} Updated {file}")
         
@@ -66,6 +66,5 @@ def update_tool(latest_version):
     except (FileNotFoundError, zipfile.BadZipFile) as e:
         print(f"{Fore.RED}[-]{Style.RESET_ALL} Error extracting the zip file: {str(e)}")
     finally:
-        # Bereinigen des temporären Verzeichnisses
         if os.path.exists("easySub_temp"):
             shutil.rmtree("easySub_temp")
